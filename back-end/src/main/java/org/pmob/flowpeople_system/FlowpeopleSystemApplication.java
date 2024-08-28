4bce2571699196d8cf9d8e97a049924e3e50c036
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-08-28 18:35:20 -0300

    feat: first commit do projeto

---
+++ back-end/src/main/java/org/pmob/flowpeople_system/FlowpeopleSystemApplication.java
@@ -0,0 +1,12 @@
+package org.pmob.flowpeople_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowpeopleSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowpeopleSystemApplication.class);
+    }
+}
