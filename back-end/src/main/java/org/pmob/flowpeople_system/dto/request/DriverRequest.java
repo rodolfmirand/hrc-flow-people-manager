5934a8199983e0134b4f6e0ca2b96e2306e4cb03
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-08-28 21:35:31 -0300

    feat: classe request para motorista

---
+++ back-end/src/main/java/org/pmob/flowpeople_system/dto/request/DriverRequest.java
@@ -0,0 +1,13 @@
+package org.pmob.flowpeople_system.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class DriverRequest {
+
+    private String name;
+
+    private String cnh;
+
+    private Character cnhType;
+}
