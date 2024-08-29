9855315ca935eb1f0b139b6b88b5be0efc754284
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-08-28 21:38:00 -0300

    feat: repository para motorista

---
+++ back-end/src/main/java/org/pmob/flowpeople_system/repository/DriverRespository.java
@@ -0,0 +1,14 @@
+package org.pmob.flowpeople_system.repository;
+
+import org.pmob.flowpeople_system.model.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface DriverRespository extends JpaRepository<Driver, UUID> {
+
+    Optional<Driver> findByCnh(String cnh);
+}
