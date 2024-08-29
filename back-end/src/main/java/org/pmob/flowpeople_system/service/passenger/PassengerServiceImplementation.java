d7edb29193675746cc0752626dae8573ecd5c35e
Rodolfo Miranda <rodolfomirandayt@gmail.com>
2024-08-28 21:39:20 -0300

    refactor: alteração da logica do service do passageiro

--- back-end/src/main/java/org/pmob/flowpeople_system/service/passenger/PassengerServiceImplementation.java
+++ back-end/src/main/java/org/pmob/flowpeople_system/service/passenger/PassengerServiceImplementation.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Service
-public class PassengerServiceImplementation implements PassengerService {
+public class PassengerServiceImplementation {
 
     @Autowired
     private PassengerRespository passengerRespository;
@@ -25,27 +25,4 @@ public class PassengerServiceImplementation implements PassengerService {
     @Autowired
     private VanRepository vanRepository;
 
-    @Override
-    public boolean register(PassengerRequest passengerRequest) {
-        Passenger passenger = passengerMapper.toPassenger(passengerRequest);
-        if (passenger != null) {
-            passengerRespository.save(passenger);
-            Van van = vanRepository.findById(passengerRequest.getIdVan()).orElse(null);
-            if (van != null) {
-                van.addPassenger(passenger);
-                vanRepository.save(van);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    @Override
-    public List<PassengerResponse> findAll(UUID idVan) {
-        Van van = vanRepository.findById(idVan).orElse(null);
-        if (van != null) {
-            return van.getPassengers() != null ? passengerMapper.toPassengerListResponse(van.getPassengers()) : null;
-        }
-        return null;
-    }
 }
