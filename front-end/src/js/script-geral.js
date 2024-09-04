7a9693763a9fc725aab512257482155085f58012
Rodolfo Oliveira Miranda <rodolfo.miranda@ourobranco.mg.gov.br>
2024-09-04 12:34:56 -0300

    novos campos de inserção na tela de novo agendamento

--- front-end/src/js/script-geral.js
+++ front-end/src/js/script-geral.js
@@ -1,5 +1,11 @@
 function limparCampos() {
-    var form = document.querySelectorAll('.inpt-form');
-    
-    form.reset();
+    var inptForm = document.querySelectorAll('.inpt-form');
+    var selc = document.querySelectorAll('.selecao');
+    var inptData = document.querySelectorAll('.inpt-form-data');
+    var inpt = document.querySelectorAll('.inpt');
+
+    inptForm.reset();
+    inptData.reset();
+    selc.reset(); 
+    inpt.reset();
 }
